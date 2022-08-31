09b8a60d20937e9459586fae611626b935dd88bb
hantl1ve <63526747+hantl1ve@users.noreply.github.com>
2022-08-31 14:45:59 +0300

    0.0.2
    Обновления:
    
    Скролл в Sidebar по hover
    Сохранение сообщений в LocaleStorage
    Поиск чатов в Sidebar

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const text = document.querySelector('.content_body_chat_input_newtext');
+const newMessage = document.querySelector('.content_body_chat_input_newtext');
 const sendIcon = document.querySelector('.content_body_chat_input_sendIcons')
 const chatArea = document.querySelector('.content_body_chat_area')
 
 // Меняем иконку 
-text.addEventListener('input', (e) => {
+newMessage.addEventListener('input', (e) => {
   if (e.target.value.length > 0) {
     sendIcon.classList.remove('record');
     sendIcon.classList.add('send');
@@ -18,67 +18,90 @@ text.addEventListener('input', (e) => {
 
 
 sendIcon.addEventListener('click', () => {
-  if (text.value.length > 0) {
+  if (newMessage.value.length > 0) {
     const hoursMinutes = new Date().getHours() + ':' + new Date().getMinutes();
+    
+    const msg = newMessage.value;
+    const msgsFromLS = localStorage.getItem('messages');
+
+    if (!msgsFromLS) {
+      localStorage.setItem('messages', JSON.stringify([msg]));
+    } else {
+      const msgsArr = JSON.parse(msgsFromLS);
+      msgsArr.push(msg);
+      localStorage.setItem('messages', JSON.stringify(msgsArr));
+    };
+
     chatArea.insertAdjacentHTML('beforeend', `
       <div class="content_body_chat_area_message_wrapper">
         <div class="content_body_chat_area_message content_body_chat_area_myMessage">
-          <span>${text.value}</span>
+          <span>${newMessage.value}</span>
           <span class="content_body_chat_area_message_time">${hoursMinutes}</span>
         </div>
       </div>
     `)
-    text.value = '';
-    text.dispatchEvent(new Event('input'));
+    newMessage.value = '';
+    newMessage.dispatchEvent(new Event('input'));
   };
 });
 
 
 // Рендерим чаты
+function renderChats (chats) {
+  const sidebarChatlist = document.querySelector('.sidebar_chatlist');
 
-const chats = [
-  {
-    avatar: './img/joker.jpg',
-    name: 'Jack Napier',
-    prevewMessage: 'Ахахахахахахахахаххахахахахахаахаха',
-    lastTime: '19:89',
-  },
-  {
-    avatar: './img/vvp.webp',
-    name: 'You know my name',
-    prevewMessage: 'Крым есть? Найду моё?',
-    lastTime: '20:14',
-  },
-  {
-    avatar: './img/Propper.jpg',
-    name: 'Propper',
-    prevewMessage: 'Готов вычистить твоё труднодоступное место',
-    lastTime: '19:58',
-  },
-  {
-    avatar: './img/sponge.jpg',
-    name: 'Sponge Bob',
-    prevewMessage: 'Я не сумасшедший, я просто немного нездоров',
-    lastTime: '19:99',
-  },
-  {
-    avatar: './img/starlord.webp',
-    name: 'Peter Quill',
-    prevewMessage: 'Давайте кое-что уточним. Этот перец — наш трофей. Все вопросы решаются через нас. Иначе решаем их мы, причем чаще всего радикально.',
-    lastTime: '19:76',
-  },
-];
+  sidebarChatlist.innerHTML = '';
 
-const sidebarChatlist = document.querySelector('.sidebar_chatlist');
-
-chats.forEach((chat) => {
-  sidebarChatlist.insertAdjacentHTML('beforeend',`
-    <div class="sidebar_chatlist_chat">
-      <img src="${chat.avatar}" alt="user avatar" class="sidebar_chatlist_chat_avatar">
-      <div class="sidebar_chatlist_chat_usertext">
-        <div class="sidebar_chatlist_chat_usertext_title"><span>${chat.name}</span><span class="sidebar_chatlist_chat_usertext_title_time">${chat.lastTime}</span></div>
-        <p class="sidebar_chatlist_chat_usertext_subtitle">${chat.prevewMessage}</p>
-      </div>
+  if (chats.length === 0) {
+    sidebarChatlist.insertAdjacentHTML('beforeend', `
+    <div class="sidebar_chatlist_noResult">
+      <p>Not result</p>
+      <span>There were not result</span>
+      <span>Try a new search</span>
     </div>
-  `)
+    `)
+  }
+
+  chats.forEach((chat) => {
+    sidebarChatlist.insertAdjacentHTML('beforeend',`
+      <div class="sidebar_chatlist_chat">
+        <img src="${chat.avatar}" alt="user avatar" class="sidebar_chatlist_chat_avatar">
+        <div class="sidebar_chatlist_chat_usertext">
+          <div class="sidebar_chatlist_chat_usertext_title"><span>${chat.name}</span><span class="sidebar_chatlist_chat_usertext_title_time">${chat.lastTime}</span></div>
+          <p class="sidebar_chatlist_chat_usertext_subtitle">${chat.prevewMessage}</p>
+        </div>
+      </div>
+    `)
+  });
+};
+renderChats(chats);
+
+// Поиск чатов
+
+const sidebarSearch = document.querySelector('.sidebar_header_input');
+
+sidebarSearch.addEventListener('input', (e) => {
+  const filterChats = chats.filter((chat) => chat.name.toLowerCase().includes(e.target.value.toLowerCase()));
+  renderChats(filterChats);
 });
+
+//Рендер сообщений из localStorage
+
+function renderMessageFromLS() {
+  const msgs = localStorage.getItem('messages');
+  if (msgs) {
+    const msgsArr = JSON.parse(msgs);
+    msgsArr.forEach((msg) => {
+    chatArea.insertAdjacentHTML('beforeend', `
+      <div class="content_body_chat_area_message_wrapper">
+        <div class="content_body_chat_area_message content_body_chat_area_myMessage">
+          <span>${msg}</span>
+          <span class="content_body_chat_area_message_time">...</span>
+        </div>
+      </div>
+    `);
+    })
+  };
+};
+
+renderMessageFromLS();
